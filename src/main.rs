981d7f127179a79c0db880798e8c3806e54e0c60
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2025-05-31 21:03:32 +0530

    fib & strings

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     println!("Hello, world!");
     println!("{}", is_even(101));
+    println!("{}", fibonnaci(10));
+    println!("{}",get_str_length(String::from("Garvit Dadheech")))
 }
 
 fn is_even(n: i32) -> bool {
@@ -8,4 +10,25 @@ fn is_even(n: i32) -> bool {
         return true;
     }
     return false;
+}
+
+fn fibonnaci(n: i32) -> i32 {
+    let mut first = 0;
+    let mut second = 1;
+    if n == 0 {
+        return 0;
+    }
+    if n ==1 {
+        return 1;
+    }
+    for _ in 1..n {
+        let temp = second;
+        second = first + second;
+        first = temp;
+    }
+    return second;
+}
+
+fn get_str_length(str: String) -> usize {
+    str.chars().count()
 }
